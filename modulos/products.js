685f11d94dff032457947315457cced18020555d
Diego <belydiego8@gmail.com>
2022-11-10 22:02:48 -0300

    modulos index.js

---
+++ modulos/products.js
@@ -0,0 +1,9 @@
+const products = [
+  {
+    id: 12,
+    nombre: "producto 12",
+    stock: 12,
+  },
+  { id: 13, nombre: "producto 13", stock: 13 },
+  { id: 14, nombre: "producto 14", stock: 14 },
+];
