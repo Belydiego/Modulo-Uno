0f0bdbf0ad0c7511b28bcf74ddc850a2a4d32ee5
Diego <belydiego8@gmail.com>
2022-11-11 16:28:40 -0300

    modulos index.js products.js

--- modulos/index.js
+++ modulos/index.js
@@ -1,2 +1,17 @@
-function main(params) {}
+// Este archivo recibe  los input del usuario y se encarga de imprimir los resultados en la terminal y con ayuda de otros modulos
+//controller
+
+const products = require("./products");
+
+function main() {
+  products.getOne(12);
+  console.log("Un producto ", products.getOne(12));
+
+  products.getAll();
+  console.log("Todos los productos", products.getAll());
+
+  products.getAllWith(13);
+  console.log("Con stock ", products.getAllWith(13));
+}
+
 main();
--- modulos/products.js
+++ modulos/products.js
@@ -1,9 +1,22 @@
 const products = [
-  {
-    id: 12,
-    nombre: "producto 12",
-    stock: 12,
-  },
+  { id: 12, nombre: "producto 12", stock: 12 },
   { id: 13, nombre: "producto 13", stock: 13 },
   { id: 14, nombre: "producto 14", stock: 14 },
 ];
+
+function getOne(id) {
+  return products.find(function (obj) {
+    return obj.id == id;
+  });
+}
+exports.getOne = getOne;
+
+exports.getAll = function () {
+  return products;
+};
+
+exports.getAllWith = function (minStock) {
+  return products.filter(function (item) {
+    return item.stock >= minStock;
+  });
+};
